e407c69bdec446813db8d389ce08e45a64607c8b
Joel M <jmjmjm439@gmail.com>
2013-09-09 18:28:20 +0200

    main stuff

--- cookieclicker/src/ch/jmnetwork/cookieclicker/CookieClickerMain.java
+++ cookieclicker/src/ch/jmnetwork/cookieclicker/CookieClickerMain.java
@@ -2,17 +2,50 @@ package ch.jmnetwork.cookieclicker;
 
 import ch.jmnetwork.cookieclicker.helper.Helper;
 
-public class CookieClickerMain 
+public class CookieClickerMain
 {
     public static CookieClickerMain INSTANCE;
+    private final static int TICKS_PER_SECOND = 25;
+    private static long lastTime = 0;
+    private static long thisTime = 0;
     
     public static void main(String[] args)
     {
         INSTANCE = new CookieClickerMain();
+        
+        while (true)
+        {
+            if (lastTime == 0) lastTime = System.nanoTime();
+            
+            thisTime = System.nanoTime();
+            
+            // only run this TICKS_PER_SECOND times per second
+            if ((thisTime - lastTime) / 1000000 >= 1000 / TICKS_PER_SECOND)
+            {
+                // ======================================//
+                // STUFF TO RUN EACH TICK
+                // ======================================//
+                
+            }
+            
+        }
+        
     }
     
     public CookieClickerMain()
     {
         Helper.registerHelpers();
+    }
+    
+    /**
+     * Convert a nanoTime to miliseconds
+     * 
+     * @param nanoTime
+     *            {@see System#nanoTime()}
+     * @return time in miliseconds
+     */
+    public int nanoToMiliseconds(long nanoTime)
+    {
+        return (int) (nanoTime / 1000000);
     }
 }
